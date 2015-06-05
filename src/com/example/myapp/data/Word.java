dfb9dd75bfee276caa519125d3cb113e303ed330
Vahagn Hakobyan <vahagn.hakobyan@iunetworks.am>
2015-06-05 17:40:37 +0400

    a few methods

--- src/com/example/myapp/data/Word.java
+++ src/com/example/myapp/data/Word.java
@@ -7,26 +7,53 @@ import com.example.myapp.common.ModelHelper;
 import com.example.myapp.data.board.GridPos;
 
 public class Word {
-	private int		x;
-	private int		y;
-	private int		length;
-	private String	tmp;
-	private String	text;
-    private String	title;
-	private String	description;
-	private boolean	horizontal = true;
-	
-	public void		setText(String value) { this.text = value; this.length = value.length(); }
-	public String	getText() { return this.text; }
 
-    public void		setTitle(String value) { this.title = value; }
-    public String	getTitle() { return this.title; }
+    private int x;
+    private int y;
+    private int length;
+    private String tmp;
+    private String text;
+    private String title;
+    private String description;
+    private boolean horizontal;
+    private boolean completed;
 
-	public void		setTmp(String value) { this.tmp = value; }
-	public String	getTmp() { return this.tmp; }
-	
-	public void		setDescription(String value) { this.description= value; }
-	public String	getDescription() { return this.description; }
+    public Word() {
+        this.horizontal = true;
+    }
+
+    public void setText(String value) {
+        this.text = value;
+        this.length = value.length();
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setTitle(String value) {
+        this.title = value;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTmp(String value) {
+        this.tmp = value;
+    }
+
+    public String getTmp() {
+        return this.tmp;
+    }
+
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
 
     public boolean isHorizontal() {
         return horizontal;
@@ -36,41 +63,80 @@ public class Word {
         this.horizontal = horizontal;
     }
 
-    public void		setX(int value) { this.x = value; }
-	public int		getX() { return this.x; }
-	public int 		getXMax() { return this.horizontal ? this.x + this.length - 1: this.x; }
-	
-	public void		setY(int value) { this.y = value; }
-	public int		getY() { return this.y; }
-	public int 		getYMax() { return this.horizontal ? this.y : this.y + this.length - 1; }
+    public void setX(int value) {
+        this.x = value;
+    }
 
-	public int 		getLength() { return this.length; }
-	
-	public List<GridPos> getGridPositions() {
-		List<GridPos> list = new ArrayList<GridPos>();
-		for(int i = 0; i < text.length(); i++) {
-			if(horizontal) {
-				list.add(new GridPos(x + i, y));
-			} else {
-				list.add(new GridPos(x, y + i));
-			}
-		}
-		return list;
-	}
-	
-	public List<Integer> getGridIndexes() {
-		List<Integer> list = new ArrayList<Integer>();
-		for(int i = 0; i < text.length(); i++) {
-			if(horizontal) {
-				list.add(ModelHelper.getGridIndex(y, x + i));
-			} else {
-				list.add(ModelHelper.getGridIndex(y + i, x));
-			}
-		}
-		return list;
-	}
+    public int getX() {
+        return this.x;
+    }
+
+    public int getXMax() {
+        return this.horizontal ? this.x + this.length - 1 : this.x;
+    }
+
+    public void setY(int value) {
+        this.y = value;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getYMax() {
+        return this.horizontal ? this.y : this.y + this.length - 1;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public List<GridPos> getGridPositions() {
+        List<GridPos> list = new ArrayList<GridPos>();
+        for (int i = 0; i < text.length(); i++) {
+            if (horizontal) {
+                list.add(new GridPos(x + i, y));
+            } else {
+                list.add(new GridPos(x, y + i));
+            }
+        }
+        return list;
+    }
+
+    public List<Integer> getGridIndexes() {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < text.length(); i++) {
+            if (horizontal) {
+                list.add(ModelHelper.getGridIndex(y, x + i));
+            } else {
+                list.add(ModelHelper.getGridIndex(y + i, x));
+            }
+        }
+        return list;
+    }
 
     public boolean isCorrect() {
         return text.equalsIgnoreCase(tmp);
+    }
+
+    public boolean hasPosition(GridPos pos) {
+        List<GridPos> positions = getGridPositions();
+        for (GridPos position : positions) {
+            if (position.getRow() == pos.getRow() && position.getCol() == pos.getCol()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getLetterAtPosition(GridPos pos) {
+        List<GridPos> positions = getGridPositions();
+        for (int i = 0; i < positions.size(); i++) {
+            GridPos position = positions.get(i);
+            if (position.getRow() == pos.getRow() && position.getCol() == pos.getCol()) {
+                return String.valueOf(text.charAt(i));
+            }
+        }
+        return "";
     }
 }
